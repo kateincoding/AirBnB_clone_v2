7b3ffa60f2a042dc4fa1bf0491595e398c235dfc
luffy981 <jhonsmith981@hotmail.com>
2021-12-22 17:01:33 -0500

    amenity added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,6 +37,7 @@ class DBStorage:
             data = self.__session.query(User).all()
             data += self.__session.query(State).all()
             data += self.__session.query(City).all()
+            data += self.__session.query(Amenity).all()
             data += self.__session.query(Place).all()
             data += self.__session.query(Review).all()
         else:
