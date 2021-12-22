9ea2fd22560b0e3c0399fd72413a73c8fd7ad8a6
luffy981 <jhonsmith981@hotmail.com>
2021-12-22 16:59:24 -0500

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime
+from os import getenv
+from sqlalchemy.orm import relationship, backref
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    """Class Ameniy"""
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class Amenity(BaseModel, Base):
+        """Class Ameniy"""
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity,
+                                       back_populates='amenities')
+else:
+    class Amenity(BaseModel, Base):
+        """Class Ameniy"""
+
     name = ""
 
