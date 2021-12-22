a3bdc9121ee3c6cbe1ecdc7091e6ae9c10f7d981
luffy981 <jhonsmith981@hotmail.com>
2021-12-22 14:24:25 -0500

    fixed user and city

--- models/city.py
+++ models/city.py
@@ -13,6 +13,8 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='city',
+                              cascade='all, delete')
 else:
     class City(BaseModel):
         """ The city class, contains state ID and name """
