de7526492c16479a52995ca91f472793481c0352
kateincoding <kateincoding@gmail.com>
2021-12-22 16:31:47 -0500

    [fix 7]

--- models/user.py
+++ models/user.py
@@ -13,10 +13,10 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="user", cascade="all, delete")
-        reviews = relationship('Review', backref='user', cascade='all, delete')
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
+        reviews = relationship("Review", backref="user", cascade="all, delete, delete-orphan")
 else:
     class User(BaseModel):
         """This class defines a user by various attributes"""
