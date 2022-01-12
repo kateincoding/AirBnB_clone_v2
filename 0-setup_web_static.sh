d4d4640a3125ff609869bdd012416c2d2fabaa88
kateincoding <kateincoding@gmail.com>
2022-01-12 18:11:52 -0500

    prepare servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#  script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Hello World" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+configure="\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t}\n"
+sudo sed -i "40i $configure" /etc/nginx/sites-available/default
+sudo service nginx restart
+sudo service nginx reload
