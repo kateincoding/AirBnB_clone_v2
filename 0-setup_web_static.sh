ceadb8b2174b0219aa720c2df8197a2ba7bb6e8b
kateincoding <kateincoding@gmail.com>
2022-01-13 06:50:00 -0500

    add \

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo chown -R ubuntu /data/web_static/releases/test
 sudo echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-configure="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
+configure="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo chown -hR ubuntu:ubuntu /data/ 
 sudo sed -i "49i\ $configure" /etc/nginx/sites-available/default
 sudo service nginx restart
