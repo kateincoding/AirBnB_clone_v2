6c14cd86e6e12fd77faddd51e59925c4be852afc
kateincoding <kateincoding@gmail.com>
2022-01-13 11:41:54 -0500

    [solve shellcheck issues]

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo chown -R ubuntu /data/web_static/releases/test
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 configure="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo chown -hR ubuntu:ubuntu /data/ 
