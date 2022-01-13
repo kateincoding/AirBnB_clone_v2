d2eeb6995999bddad616835ce299250fb215fbe0
kateincoding <kateincoding@gmail.com>
2022-01-13 06:40:11 -0500

    improve script & norm

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#  script that sets up your web servers for the deployment of web_static
+# script that sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -R ubuntu /data/web_static/releases/test
 sudo echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 configure="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
