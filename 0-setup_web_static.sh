3b0cf915922bdde5dbd267a1ff066236660bc487
kateincoding <kateincoding@gmail.com>
2022-01-13 06:58:27 -0500

    return

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,6 @@ sudo chown -R ubuntu /data/web_static/releases/test
 sudo echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 configure="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo useradd ubuntu
 sudo chown -hR ubuntu:ubuntu /data/ 
 sudo sed -i "49i\ $configure" /etc/nginx/sites-available/default
 sudo service nginx restart
