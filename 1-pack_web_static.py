4d01fcfca24d12b4c8862450456c1a7f728ecc50
kateincoding <kateincoding@gmail.com>
2022-01-13 12:00:15 -0500

    [another simple solution]

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,17 +2,14 @@
 """script that generates a tgz archive"""
 from fabric.api import *
 from datetime import datetime
-import os
 
 
 def do_pack():
     """function that generates tgz archive"""
     try:
-        dir1 = "/versions/"
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         file_name = "versions/web_static_{}.tgz".format(date)
-        if os.path.exists(dir1) is False:
-            local("mkdir versions")
+        local("mkdir -p versions")
         local('tar -cvzf {} web_static'.format(file_name))
         return file_name
     except Exception as e:
