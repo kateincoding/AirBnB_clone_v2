a002882620bc5c524688db5ff025d6b0754d5f9a
kateincoding <kateincoding@gmail.com>
2022-01-13 06:29:38 -0500

    fixed chown

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 sudo echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 configure="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
