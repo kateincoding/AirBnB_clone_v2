4b8418fe2810ccfad0af484cd0574e6911ca7248
kateincoding <kateincoding@gmail.com>
2022-01-30 00:22:03 -0500

    [add header, add debug, fix pycodestyle]

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,6 @@
-from flask import Flask
+#!/usr/bin/python3
 """module hello route of flask"""
+from flask import Flask
 app = Flask(__name__)
 
 
@@ -9,5 +10,5 @@ def hello_world():
     return 'Hello HBNB!'
 
 
-if __name__=='__main__':
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
