9ad69bfd62394e3df294fdb8db6c14aa1cb58761
kateincoding <kateincoding@gmail.com>
2022-01-30 14:10:02 -0500

    cities by state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module flask"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """Close SLQAlchemy session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def show_states_list():
+    """html that shows the list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
