672e4d2f0668d5d00bdf816c0bfba97e02d41d2c
kateincoding <kateincoding@gmail.com>
2022-01-30 19:17:02 -0500

    [list cities]

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,8 +15,8 @@ def teardown_db(self):
 @app.route('/states_list', strict_slashes=False)
 def show_states_list():
     """html that shows the list of states"""
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
